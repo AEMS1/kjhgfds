323b893c9354cf081676125fbf3e72669889751a
AEMS1 <aems91806@gmail.com>
2025-05-30 15:18:02 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,24 +1,11 @@
-const contractABI = [
+const abi = [
   {
     "inputs": [
-      { "internalType": "address", "name": "from", "type": "address" },
-      { "internalType": "address", "name": "to", "type": "address" },
-      { "internalType": "uint256", "name": "amount", "type": "uint256" }
+      { "internalType": "address", "name": "_tokenAddress", "type": "address" },
+      { "internalType": "address", "name": "_feeWallet", "type": "address" }
     ],
-    "name": "transferFrom",
-    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
     "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "recipient", "type": "address" },
-      { "internalType": "uint256", "name": "amount", "type": "uint256" }
-    ],
-    "name": "transfer",
-    "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    "type": "constructor"
   },
   {
     "inputs": [{ "internalType": "uint256", "name": "gameId", "type": "uint256" }],
@@ -50,5 +37,3 @@ const contractABI = [
     "type": "function"
   }
 ];
-
-const contractAddress = "0xa8F88821F9A5Ea4Fef29c41Ca191290415e9FaF8";
