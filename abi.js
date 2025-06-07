3600dd582f6879f96fc1956cfcfdbacc67abf5f5
AEMS1 <aems91806@gmail.com>
2025-06-07 13:52:45 +0330

    Delete abi.js

--- abi.js
+++
@@ -1,5 +0,0 @@
-const contractABI = [
-  { "inputs": [], "name": "enterGame", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-  { "inputs": [], "name": "claimReward", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
-  { "inputs": [], "name": "resign", "outputs": [], "stateMutability": "nonpayable", "type": "function" }
-];
