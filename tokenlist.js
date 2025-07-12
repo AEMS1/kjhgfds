c58ad7f06063883414bfe30579d03d7be6085c7d
AEMS1 <aems91806@gmail.com>
2025-07-12 06:59:06 +0330

    Create tokenlist.js

---
+++ tokenlist.js
@@ -0,0 +1,20 @@
+const tokenList = [
+  {
+    name: "BNB",
+    address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    symbol: "BNB",
+    decimals: 18
+  },
+  {
+    name: "AMS Token",
+    address: "0x887ada8fe79740b913De549f81014f37e2f8D07a",
+    symbol: "AMS",
+    decimals: 18
+  },
+  {
+    name: "USDT",
+    address: "0x55d398326f99059fF775485246999027B3197955",
+    symbol: "USDT",
+    decimals: 18
+  }
+];
