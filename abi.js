14cb35be0ab76d956583a48ad668bc684f584a23
AEMS1 <aems91806@gmail.com>
2025-07-12 07:02:44 +0330

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,3 +1,4 @@
+
 const pancakeRouterAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E"; // Pancake Router V2
 const pancakeRouterAbi = [ // simplified ABI for swapExactTokensForTokens and swapExactETHForTokens
   "function getAmountsOut(uint amountIn, address[] memory path) view returns (uint[] memory amounts)",
@@ -11,4 +12,3 @@ const erc20Abi = [
   "function balanceOf(address account) external view returns (uint256)",
   "function decimals() external view returns (uint8)"
 ];
-
